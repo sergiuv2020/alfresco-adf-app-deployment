0d7638deca05009345054bccd30b5114e4f75147
Marcello Teodori <marcello.teodori@alfresco.com>
2018-10-10 22:34:20 +0100

    ACTIVITI-1632 add support for path without hostName

--- install-alfresco-admin-app.sh
+++ install-alfresco-admin-app.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-helm install --name alfresco-admin-app \
-    --set image.repository=quay.io/alfresco/alfresco-admin-app \
-    --set ingress.hostName=apa-aps2dev.envalfresco.com \
-    ./helm/alfresco-adf-app
+helm upgrade --install \
+  --set image.repository=quay.io/alfresco/alfresco-admin-app \
+  --set ingress.hostName=apa-aps2dev.envalfresco.com \
+  alfresco-admin-app ./helm/alfresco-adf-app
--- install-alfresco-modeler-app.sh
+++ install-alfresco-modeler-app.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-helm install --name alfresco-modeler-app \
-    --set image.repository=quay.io/alfresco/alfresco-modeler-app \
-    --set ingress.hostName=ama-aps2dev.envalfresco.com \
-    ./helm/alfresco-adf-app
+helm upgrade --install \
+  --set image.repository=quay.io/alfresco/alfresco-modeler-app \
+  --set ingress.hostName= \
+  --set ingress.path=/alfresco-modeler-app \
+  --set env.BASE_PATH=/alfresco-modeler-app/ \
+  alfresco-modeler-app ./helm/alfresco-adf-app
