8af899a97b0ceefe442eec90dd04833039e0bedb
Marcello Teodori <marcello.teodori@alfresco.com>
2018-10-16 20:33:25 +0100

    ACTIVITI-1632 updated image for content

--- install-alfresco-content-app.sh
+++ install-alfresco-content-app.sh
@@ -3,13 +3,14 @@
 BASE_URL="/alfresco-content-app"
 
 helm upgrade --install --wait \
+  --set registryPullSecrets=quay-registry-secret \
   --set image.repository=quay.io/alfresco/alfresco-content-app \
-  --set image.tag=dev-mteodori-ACA-174 \
+  --set image.tag=0.1.5-beta \
   --set image.pullPolicy=Always \
   --set ingress.path="${BASE_URL}" \
   --set env.BASEPATH=./ \
-  --set env.API_URL="http://aps2test.envalfresco.com" \
+  --set env.API_URL="http://localhost" \
   --set env.APP_CONFIG_AUTH_TYPE="OAUTH" \
-  --set env.APP_CONFIG_OAUTH2_HOST="http://aps2test.envalfresco.com/auth/realms/springboot" \
+  --set env.APP_CONFIG_OAUTH2_HOST="http://localhost/auth/realms/springboot" \
   --set env.APP_CONFIG_OAUTH2_CLIENTID="activiti" \
   alfresco-content-app ./helm/alfresco-adf-app
