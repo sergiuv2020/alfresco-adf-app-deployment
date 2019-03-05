4c64f380837425927dfd08e1587635a2b62c2e17
Ryan Dawson <ryan.dawson@alfresco.com>
2019-03-05 11:26:19 +0000

    Update install-alfresco-modeling-app.sh for https://github.com/Activiti/activiti-modeling-app/pull/74/files

--- install-alfresco-modeling-app.sh
+++ install-alfresco-modeling-app.sh
@@ -7,7 +7,7 @@ helm upgrade --install --wait ${HELM_OPTS} \
   --set image.repository=quay.io/alfresco/alfresco-modeling-app \
   --set image.pullPolicy=Always \
   --set ingress.path="/alfresco-modeling-app" \
-  --set env.API_URL="${ENV_URL}" \
+  --set env.APP_CONFIG_BPM_HOST="${ENV_URL}" \
   --set env.APP_CONFIG_AUTH_TYPE="OAUTH" \
   --set env.APP_CONFIG_OAUTH2_HOST="${ENV_URL}/auth/realms/alfresco" \
   --set env.APP_CONFIG_OAUTH2_CLIENTID="activiti" \
