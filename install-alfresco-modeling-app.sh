9585bb84f80ad56127ef99feab4361e9487e3f5e
Maurizio Vitale <maurizio.vitale@alfresco.com>
2020-01-23 11:38:48 +0000

    [AAE-1111] - Override the IDENTITY_HOST value

--- install-alfresco-modeling-app.sh
+++ install-alfresco-modeling-app.sh
@@ -2,10 +2,18 @@
 
 APP_NAME=alfresco-modeling-app
 
+cat <<EOF > .values.yaml
+  env:
+    APP_CONFIG_IDENTITY_HOST: "{{ include \"common.keycloak-url\" . }}/admin/realms/{{ include \"common.keycloak-realm\" . }}"
+EOF
+
 HELM_OPTS="
+  -f .values.yaml
   --set global.registryPullSecrets={quay-registry-secret}
   --set image.repository=quay.io/alfresco/${APP_NAME}
   ${HELM_OPTS}
 "
 
 . $(dirname "$0")/install.sh
+
+rm .values.yaml
