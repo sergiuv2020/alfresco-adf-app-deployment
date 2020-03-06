ee44bc4e209ac0f7f86a9824dcbe1e061d721642
Sohel Saiyed <sohel.saiyed@alfresco.com>
2020-03-06 15:20:30 +0000

    [AAE-2096]-  update modeling-app internal port to 8080 (#4)
    [AAE-2096]

--- install-alfresco-modeling-app.sh
+++ install-alfresco-modeling-app.sh
@@ -5,6 +5,7 @@ APP_NAME=alfresco-modeling-app
 HELM_OPTS="
   --set global.registryPullSecrets={quay-registry-secret}
   --set image.repository=quay.io/alfresco/${APP_NAME}
+  --set image.internalPort=8080
   ${HELM_OPTS}
 "
 
