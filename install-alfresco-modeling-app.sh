38a16d4ad6e1330bd96f5fb01734f5c52ab19c7b
Miguel Ruiz <miguel.ruiz.developer@gmail.com>
2020-04-21 17:51:47 +0100

    Fix deployment to reflect the change in values (#9)

--- install-alfresco-modeling-app.sh
+++ install-alfresco-modeling-app.sh
@@ -5,7 +5,6 @@ APP_NAME=alfresco-modeling-app
 HELM_OPTS="
   --set global.registryPullSecrets={quay-registry-secret}
   --set image.repository=quay.io/alfresco/${APP_NAME}
-  --set image.internalPort=8080
   ${HELM_OPTS}
 "
 
